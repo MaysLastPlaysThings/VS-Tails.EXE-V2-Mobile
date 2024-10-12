715ae188b2439fa6703f79294b2dec6691048709
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-10-12 13:38:40 +0300

    yes

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -34,56 +34,52 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 	public function new()
 	{
 		title = 'Graphics';
-		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
+		rpcTitle = 'Graphics Settings Menu'; // for Discord Rich Presence
 
-		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
-		var option:Option = new Option('Low Quality', //Name
-			'If checked, disables some background details,\ndecreases loading times and improves performance.', //Description
-			'lowQuality', //Save data variable name
-			'bool', //Variable type
-			false); //Default value
+		// I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
+		var option:Option = new Option('Low Quality', // Name
+			'If checked, disables some background details,\ndecreases loading times and improves performance.', // Description
+			'lowQuality', // Save data variable name
+			'bool', // Variable type
+			false); // Default value
 		addOption(option);
 
-		var option:Option = new Option('Anti-Aliasing',
-			'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
-			'globalAntialiasing',
-			'bool',
-			true);
+		var option:Option = new Option('Anti-Aliasing', 'If unchecked, disables anti-aliasing, increases performance\nat the cost of sharper visuals.',
+			'globalAntialiasing', 'bool', true);
 		option.showBoyfriend = true;
-		option.onChange = onChangeAntiAliasing; //Changing onChange is only needed if you want to make a special interaction after it changes the value
+		option.onChange = onChangeAntiAliasing; // Changing onChange is only needed if you want to make a special interaction after it changes the value
 		addOption(option);
 
-		var option:Option = new Option('Shaders', //Name
-			'If unchecked, disables shaders.\nIt\'s used for some visual effects, and also CPU intensive for weaker PCs. (Also disabled Bloom)', //Description
-			'shaders', //Save data variable name
-			'bool', //Variable type
-			true); //Default value
+		var option:Option = new Option('Shaders', // Name
+			'If unchecked, disables shaders.\nIt\'s used for some visual effects, and also CPU intensive for weaker PCs. (Also disabled Bloom)', // Description
+			'shaders', // Save data variable name
+			'bool', // Variable type
+			true); // Default value
 		option.onChange = onChangeShaders;
 		addOption(option);
-		
 
-		var option:Option = new Option('Bloom', //Name
-			'If unchecked, disables bloom.\nIt\'s a visual effect. If shaders are disabled, same will happen to this.', //Description
-			'bloom', //Save data variable name
-			'bool', //Variable type
-			true); //Default value
+		var option:Option = new Option('Bloom', // Name
+			'If unchecked, disables bloom.\nIt\'s a visual effect. If shaders are disabled, same will happen to this.', // Description
+			'bloom', // Save data variable name
+			'bool', // Variable type
+			true); // Default value
 		addOption(option);
 
 		/*
-		#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
-		var option:Option = new Option('Framerate',
-			"Pretty self explanatory, isn't it?",
-			'framerate',
-			'int',
-			60);
-		addOption(option);
+			#if !html5 //Apparently other framerates isn't correctly supported on Browser? Probably it has some V-Sync shit enabled by default, idk
+			var option:Option = new Option('Framerate',
+				"Pretty self explanatory, isn't it?",
+				'framerate',
+				'int',
+				60);
+			addOption(option);
 
-		option.minValue = 60;
-		option.maxValue = 240;
-		option.displayFormat = '%v FPS';
-		option.onChange = onChangeFramerate;
-		#end
-		*/ //aaaaaaaaaaaaaaaaand framerates higher than 60 crash the game
+			option.minValue = 60;
+			option.maxValue = 240;
+			option.displayFormat = '%v FPS';
+			option.onChange = onChangeFramerate;
+			#end
+		 */ // aaaaaaaaaaaaaaaaand framerates higher than 60 crash the game
 
 		super();
 	}
@@ -92,9 +88,10 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 	{
 		for (sprite in members)
 		{
-			var sprite:Dynamic = sprite; //Make it check for FlxSprite instead of FlxBasic
-			var sprite:FlxSprite = sprite; //Don't judge me ok
-			if(sprite != null && (sprite is FlxSprite) && !(sprite is FlxText)) {
+			var sprite:Dynamic = sprite; // Make it check for FlxSprite instead of FlxBasic
+			var sprite:FlxSprite = sprite; // Don't judge me ok
+			if (sprite != null && (sprite is FlxSprite) && !(sprite is FlxText))
+			{
 				sprite.antialiasing = ClientPrefs.globalAntialiasing;
 			}
 		}
@@ -102,13 +99,13 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 
 	function onChangeShaders()
 	{
-		if(!ClientPrefs.shaders)
+		if (!ClientPrefs.shaders)
 			ClientPrefs.bloom = false;
 	}
 
 	function onChangeFramerate()
 	{
-		if(ClientPrefs.framerate > FlxG.drawFramerate)
+		if (ClientPrefs.framerate > FlxG.drawFramerate)
 		{
 			FlxG.updateFramerate = ClientPrefs.framerate;
 			FlxG.drawFramerate = ClientPrefs.framerate;
--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -30,24 +30,52 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = [
+		'Note Colors',
+		'Controls',
+		'Adjust Delay and Combo',
+		'Graphics',
+		'Visuals and UI',
+		'Gameplay'
+	];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
+
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
 
-	function openSelectedSubstate(label:String) {
-		switch(label) {
+	function openSelectedSubstate(label:String)
+	{
+		switch (label)
+		{
 			case 'Note Colors':
+				#if mobile
+				removeVirtualPad();
+				#end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
+				#if mobile
+				removeVirtualPad();
+				#end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
+				#if mobile
+				removeVirtualPad();
+				#end
 				openSubState(new options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
+				#if mobile
+				removeVirtualPad();
+				#end
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
+				#if mobile
+				removeVirtualPad();
+				#end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
+				#if mobile
+				removeVirtualPad();
+				#end
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 		}
 	}
@@ -55,33 +83,34 @@ class OptionsState extends MusicBeatState
 	var selectorLeft:Alphabet;
 	var selectorRight:Alphabet;
 
-	override function create() {
+	override function create()
+	{
 		#if desktop
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
 		/*
 
-		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
-		bg.color = 0xFFea71fd;
-		bg.updateHitbox();
+			var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+			bg.color = 0xFFea71fd;
+			bg.updateHitbox();
 
-		bg.screenCenter();
-		bg.antialiasing = ClientPrefs.globalAntialiasing;
-		add(bg);
+			bg.screenCenter();
+			bg.antialiasing = ClientPrefs.globalAntialiasing;
+			add(bg);
 
-		*/
-		
+		 */
+
 		var bg:FlxBackdrop;
 		#if (flixel > "5.0.0")
 		bg = new FlxBackdrop(Paths.image("menustuff/greyd", 'sadfox'), XY, 0, 0);
 		#else
 		bg = new FlxBackdrop(Paths.image("menustuff/greyd", 'sadfox'), 8, 8, true, true, 1, 1);
 		#end
-        bg.velocity.set(FlxG.random.bool(50) ? 90 : -90, FlxG.random.bool(50) ? 90 : -90);
+		bg.velocity.set(FlxG.random.bool(50) ? 90 : -90, FlxG.random.bool(50) ? 90 : -90);
 		bg.screenCenter();
-        //bg.alpha = 0.4;
-        add(bg);
+		// bg.alpha = 0.4;
+		add(bg);
 
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
@@ -102,35 +131,73 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+		#if mobile
+		addVirtualPad(UP_DOWN, A_B_X_Y);
+		#end
+
+		#if mobile
+		var sus:FlxText = new FlxText(10, 14, 0, 'Press X to customize your android controls', 16);
+		sus.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sus.borderSize = 2.4;
+		sus.scrollFactor.set();
+		add(sus); // sus??!?!
+
+		var sussy:FlxText = new FlxText(10, 32, 0, 'Press Y to customize opacity for your android controls', 16);
+		sussy.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sussy.borderSize = 2.4;
+		sussy.scrollFactor.set();
+		add(sussy); // even more sus!
+		#end
+
 		super.create();
 	}
 
-	override function closeSubState() {
+	override function closeSubState()
+	{
 		super.closeSubState();
 		ClientPrefs.saveSettings();
 	}
 
-	override function update(elapsed:Float) {
+	override function update(elapsed:Float)
+	{
 		super.update(elapsed);
 
-		if (controls.UI_UP_P) {
+		#if mobile
+		if (virtualPad.buttonX.justPressed)
+		{
+			removeVirtualPad();
+			openSubState(new mobile.MobileControlsSubState());
+		}
+		if (virtualPad.buttonY.justPressed)
+		{
+			removeVirtualPad();
+			openSubState(new mobile.AndroidSettingsSubState());
+		}
+		#end
+
+		if (controls.UI_UP_P)
+		{
 			changeSelection(-1);
 		}
-		if (controls.UI_DOWN_P) {
+		if (controls.UI_DOWN_P)
+		{
 			changeSelection(1);
 		}
 
-		if (controls.BACK) {
+		if (controls.BACK)
+		{
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 			MusicBeatState.switchState(new MainMenuState());
 		}
 
-		if (controls.ACCEPT) {
+		if (controls.ACCEPT)
+		{
 			openSelectedSubstate(options[curSelected]);
 		}
 	}
-	
-	function changeSelection(change:Int = 0) {
+
+	function changeSelection(change:Int = 0)
+	{
 		curSelected += change;
 		if (curSelected < 0)
 			curSelected = options.length - 1;
@@ -139,12 +206,14 @@ class OptionsState extends MusicBeatState
 
 		var bullShit:Int = 0;
 
-		for (item in grpOptions.members) {
+		for (item in grpOptions.members)
+		{
 			item.targetY = bullShit - curSelected;
 			bullShit++;
 
 			item.alpha = 0.6;
-			if (item.targetY == 0) {
+			if (item.targetY == 0)
+			{
 				item.alpha = 1;
 				selectorLeft.x = item.x - 63;
 				selectorLeft.y = item.y;
